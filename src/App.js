71deebe5a8697185440885641606c33c656db513
daoduyphuong <daoduyphuong0995@gmail.com>
2018-05-25 17:39:47 +0700

    Oop! There's something wrong. `react-script` is not found

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Repo from './Repo';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+      <Repo title="parcel-bundler/parcel" body="📦🚀 Blazing fast, zero configuration web application bundler"
+      fork_num="908" comment_num="390" star_num="33476" />
     );
   }
 }
---
+++ src/Repo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import FontAwesome from 'font-awesome';
+import './Repo.css';
+
+class Repo extends Component {
+
+    render() {
+        return (
+            <section className="repo">
+                <h2 className="repo__title">{this.props.title}</h2>
+                <p className="repo__body">{this.props.body}</p>
+                <div className="repo__footer">
+                    <span><i className="fa fa-code-fork"></i> {this.props.fork_num}</span>
+                    <span><i className="fa fa-commenting-o"></i> {this.props.comment_num}</span>
+                    <span><i className="fa fa-star-o"></i> {this.props.star_num}</span>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Repo;
